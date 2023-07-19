7a3224eb8098de7b16e886653f51beccef461ba3
francisco fernandez <fco.fernandez.ag@gmail.com>
2023-07-19 18:39:37 -0400

    3era pre-entrega ok

--- Tercera Pre-Entrega/src/index.js
+++ Tercera Pre-Entrega/src/index.js
@@ -1,11 +1,12 @@
 import "dotenv/config";
 import express from "express";
+import session from 'express-session';
+import { Server } from 'socket.io'
 import { engine } from 'express-handlebars';
 import path from 'path';
+import passport from './controllers/githubAuth.js';
 import cookieParser from 'cookie-parser';
 import MongoStore from 'connect-mongo'
-import session from 'express-session';
-
 import { __dirname, __filename } from './path.js'
 import { authenticate } from './middlewares.js';
 import { connectDatabase } from './database.js';
@@ -14,13 +15,16 @@ import { registerUser } from "./controllers/localAuth.js";
 import productRouter from "./routes/product.routes.js";
 import cartRouter from "./routes/cart.routes.js";
 import adminRouter from "./routes/admin.routes.js";
-import passport from './controllers/githubAuth.js';
 import messagesRouter from "./routes/messages.routes.js";
 
 const app = express();
 const PORT = 4000;
 
-// Middleware
+const server = app.listen(PORT, () => {
+  console.log(`Server on port ${PORT}`)
+})
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.SIGNED_COOKIE));
@@ -34,6 +38,21 @@ app.use(session({
   resave: true,
   saveUninitialized: true
 }));;
+
+
+//ServerIO
+
+const io = new Server(server)
+const mensajes = []
+
+io.on('connection', (socket) => {
+    console.log("Cliente conectado")
+    socket.on("mensaje", info => {
+        console.log(info)
+        mensajes.push(info)
+        io.emit("mensajes", mensajes) //Le envio todos los mensajes guardados
+    })
+})
 
 // Handlebars
 app.engine(
@@ -94,32 +113,14 @@ app.get('/auth/github/callback', passport.authenticate('github', { failureRedire
 app.use('/products', authenticate, productRouter);
 app.use('/carts', authenticate, cartRouter);
 app.use('/admin', authenticate, adminRouter);
-app.use('/message', messagesRouter);
+app.use('/message',authenticate, messagesRouter);
+app.use('/chat',authenticate, express.static(__dirname + '/public')) //Defino la ruta de mi carpeta publica
 
-
-app.listen(PORT, () => {
-  console.log(`Server on port ${PORT}`);
+//vista chat
+app.get("/chat", (req, res) => {
+  const cartId = req.session.user.cartId; 
+  res.render('chat', { cartId: cartId }); 
 });
 
 
-//LOGIN
-
-  // Prueba 1 -Se crea pagina de login y Registro
-            //URL: http://localhost:4000/
-            // usuario por defecto "fernandezfco@hotmail.cl" Pass" 123456". 
-            // Para probar el registro se solicita crear tu propio usuario e ingresar con el.
-
-  // Prueba 2 - Al ingresar con usuario logeado mostrara vista de los productos
-        //URL: http://localhost:4000/products   ( corroborar que sin logearse no se puede acceder directamente a esta ruta)
-      
- // Prueba 3 - Se implementa boton de cerrar sesion para "destruir sesion y redirigir al login"
-
-// Prueba 4 - En la vista login se puede filtrar por titulo , ordenar por precio y correr paginas. Se mantiene la configuracion del desafio anterior de un limite 
-// de 10 productos por pagina.
-
-
-// // AGREGO PRIMEROS USUARIOS A MODELO USERS
-// await userModel.insertMany([
-//     { first_name: "Francisco", last_name: "Fernandez", email: "fernandezfco@hotmail.cl", gender: "Masculino", rol: "Administrador",  password: "123456" },
-// ])
 
