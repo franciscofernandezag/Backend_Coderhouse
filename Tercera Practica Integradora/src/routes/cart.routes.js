02bdac8713196350e21752dbc878a74f9339bceb
francisco fernandez <fco.fernandez.ag@gmail.com>
2023-08-09 18:34:24 -0400

    premium no puede ocmprar sus productos pendiente

--- Tercera Practica Integradora/src/routes/cart.routes.js
+++ Tercera Practica Integradora/src/routes/cart.routes.js
@@ -3,7 +3,7 @@ import CartModel from "../models/Carts.js";
 import ProductModel from "../models/Products.js";
 import purchaseModel from "../models/Purchase.js";
 import { sumarProductosIguales, calcularTotal } from "../utils/utilsCarts.js";
-import { loggerDev, loggerProd } from  "../utils/logger.js";
+import { loggerDev, loggerProd } from "../utils/logger.js";
 
 const cartRouter = Router();
 
@@ -19,12 +19,22 @@ cartRouter.get("/:cartId", async (req, res) => {
     const summedProducts = sumarProductosIguales(cart.products); // Agrega esta línea para obtener la lista de productos sumados
     const message = req.session.message; // Obtén el mensaje de la sesión
     req.session.message = null; // Limpia el mensaje de la sesión después de obtenerlo
-    loggerProd.info(`Carrito con ID ${cartId} obtenido satisfactoriamente. Cantidad de productos en el carrito: ${summedProducts.length}`);
+    loggerProd.info(
+      `Carrito con ID ${cartId} obtenido satisfactoriamente. Cantidad de productos en el carrito: ${summedProducts.length}`
+    );
 
-    res.render("carts", { cart: cart, cartId: cartId, products: summedProducts, total: calcularTotal(summedProducts), message: message }); 
+    res.render("carts", {
+      cart: cart,
+      cartId: cartId,
+      products: summedProducts,
+      total: calcularTotal(summedProducts),
+      message: message,
+    });
   } catch (error) {
     loggerProd.fatal("Error al obtener los productos del carrito:", error);
-    res.status(500).json({ error: "Error al obtener los productos del carrito" });
+    res
+      .status(500)
+      .json({ error: "Error al obtener los productos del carrito" });
   }
 });
 
@@ -44,7 +54,9 @@ cartRouter.post("/:cartId/products/:productId", async (req, res) => {
       return res.status(404).json({ error: "Producto no encontrado" });
     }
 
-    const existingProduct = cart.products.find((item) => item.id.equals(productId));
+    const existingProduct = cart.products.find((item) =>
+      item.id.equals(productId)
+    );
 
     if (existingProduct) {
       // El producto ya existe en el carrito, actualiza la cantidad
@@ -56,15 +68,18 @@ cartRouter.post("/:cartId/products/:productId", async (req, res) => {
 
     await cart.save();
     req.session.message = "Se ha agregado un producto al carrito.";
-    loggerProd.info(`Producto con ID ${productId} agregado o actualizado en el carrito con ID ${cartId}`);
+    loggerProd.info(
+      `Producto con ID ${productId} agregado o actualizado en el carrito con ID ${cartId}`
+    );
 
-    res.redirect(`/products?message=${encodeURIComponent(req.session.message)}`);
+    res.redirect(
+      `/products?message=${encodeURIComponent(req.session.message)}`
+    );
   } catch (error) {
     loggerProd.fatal("Error al agregar producto al carrito:", error);
     res.status(500).json({ error: "Error al agregar producto al carrito" });
   }
 });
-
 
 // Eliminar todos los productos del carrito
 cartRouter.get("/:cartId/products", async (req, res) => {
@@ -80,15 +95,15 @@ cartRouter.get("/:cartId/products", async (req, res) => {
     if (!cart) {
       return res.status(404).json({ error: "Carrito no encontrado" });
     }
-    loggerProd.info(`Todos los productos eliminados del carrito con ID ${cartId}`);
+    loggerProd.info(
+      `Todos los productos eliminados del carrito con ID ${cartId}`
+    );
     res.redirect(`/carts/${cartId}`);
-
   } catch (error) {
     loggerProd.fatal("Error al eliminar productos del carrito:", error);
     res.status(500).json({ error: "Error al eliminar productos del carrito" });
   }
 });
-
 
 // Eliminar  un  productos del producto a un carrito en vase a su ID
 cartRouter.post("/:cartId/products/:productId/delete", async (req, res) => {
@@ -113,7 +128,9 @@ cartRouter.post("/:cartId/products/:productId/delete", async (req, res) => {
     cart.products.splice(productIndex, 1);
     await cart.save();
     req.session.message = "Has eliminado un producto.";
-    loggerProd.info(`Producto con ID ${productId} eliminado del carrito con ID ${cartId}`);
+    loggerProd.info(
+      `Producto con ID ${productId} eliminado del carrito con ID ${cartId}`
+    );
 
     res.redirect(`/carts/${cartId}`);
   } catch (error) {
@@ -157,8 +174,10 @@ cartRouter.post("/:cartId/purchase", async (req, res) => {
           Id: productFromDB._id,
           quantity: quantity,
           price: productFromDB.price,
-          subtotal: subtotal
+          subtotal: subtotal,
         });
+
+        
       } else {
         successful = false;
         productsToPurchase.push({
@@ -166,7 +185,7 @@ cartRouter.post("/:cartId/purchase", async (req, res) => {
           Id: productFromDB._id,
           quantity: quantity,
           price: productFromDB.price,
-          subtotal: productFromDB.price * quantity
+          subtotal: productFromDB.price * quantity,
         });
       }
     }
@@ -187,30 +206,40 @@ cartRouter.post("/:cartId/purchase", async (req, res) => {
       successful: successful,
       total: successful ? total : 0,
       ticket: purchase.ticket,
-      purchaseDate: Date.now()
+      purchaseDate: Date.now(),
     });
     if (successful) {
       // Todos los productos tienen suficiente stock
       // Guardar los cambios en los productos y vaciar el carrito
-      await Promise.all(productsToPurchase.map(item => item.product.save()));
+      await Promise.all(productsToPurchase.map((item) => item.product.save()));
       cart.products = [];
       await cart.save();
-      req.session.products = productsToPurchase; 
+      req.session.products = productsToPurchase;
       const cartId = req.session.user.cartId;
-      loggerProd.info(`Compra finalizada con éxito para el carrito con ID ${cartId}. Ticket de compra: ${purchase.ticket}`);
-      return res.render("purchase-successful", { products: productsToPurchase, total: total, cartId: cartId });
+      loggerProd.info(
+        `Compra finalizada con éxito para el carrito con ID ${cartId}. Ticket de compra: ${purchase.ticket}`
+      );
+      return res.render("purchase-successful", {
+        products: productsToPurchase,
+        total: total,
+        cartId: cartId,
+      });
     } else {
-
-      req.session.products = productsToPurchase; 
+      req.session.products = productsToPurchase;
       const cartId = req.session.user.cartId;
-      logger.info(`Compra no se puedo realizar para el carrito con ID ${cartId}. Ticket de compra inconclusa : ${purchase.ticket}`);
-      return res.render("purchase-failed", { products: productsToPurchase, total: total, cartId: cartId });
+      loggerProd.info(
+        `Compra no se puedo realizar para el carrito con ID ${cartId}. Ticket de compra inconclusa : ${purchase.ticket}`
+      );
+      return res.render("purchase-failed", {
+        products: productsToPurchase,
+        total: total,
+        cartId: cartId,
+      });
     }
   } catch (error) {
     loggerProd.fatal("Error al finalizar la compra:", error);
     res.status(500).json({ error: "Error al finalizar la compra" });
   }
 });
-
 
 export default cartRouter;
