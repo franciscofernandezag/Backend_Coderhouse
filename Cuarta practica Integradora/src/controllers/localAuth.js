e99019ad8515fee27a0dd11b1f7300a8a3a7f39a
francisco fernandez <fco.fernandez.ag@gmail.com>
2023-08-31 15:25:42 -0400

    arreglo de vistas

---
+++ Cuarta practica Integradora/src/controllers/localAuth.js
@@ -0,0 +1,74 @@
+import { userModel } from "../dao/models/Users.js";
+import cartModel from "../dao/models/Carts.js";
+import { comparePasswords, hashPassword } from "../utils/bcryptUtils.js";
+import passport from '../utils/passportUtils.js';
+
+export async function loginUser(req, res, next) {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      console.error('Error en el inicio de sesión:', err);
+      return res.status(500).render('home', { title: 'Página de inicio', error: 'Error en el servidor' });
+    }
+
+    if (!user) {
+      return res.render('home', { title: 'Página de inicio', error: info.message });
+    }
+
+    req.logIn(user, (err) => {
+      if (err) {
+        console.error('Error en el inicio de sesión:', err);
+        return res.status(500).render('home', { title: 'Página de inicio', error: 'Error en el servidor' });
+      }
+
+      req.session.user = user;
+      req.session.user.rol = user.rol;
+
+      // Verificarrol de usuario 
+      if (user.rol === 'administrador') {
+        return res.redirect('/admin'); 
+      } else if (user.rol === 'premium') {
+        return res.redirect('/premium'); 
+      } else {
+        return res.redirect('/products'); 
+      }
+    });
+  })(req, res, next);
+}
+export async function registerUser(req, res) {
+  const { nombre, apellido, email, genero, rol, password } = req.body;
+  try {
+    if (!nombre || !apellido || !email || !genero || !rol || !password) {
+      return res.status(400).render('home', { title: 'Página de inicio', error: 'Faltan campos obligatorios' });
+    }
+
+    // Comprobar si el email ya está en uso
+    const existingUser = await userModel.findOne({ email });
+    if (existingUser) {
+      return res.status(400).render('home', { title: 'Página de inicio', error: 'Email de usuario en uso' });
+    }
+
+    // Generar el hash de la contraseña
+    const hashedPassword = await hashPassword(password);
+    const user = await userModel.create({
+      first_name: nombre,
+      last_name: apellido,
+      email,
+      gender: genero,
+      rol,
+      password: hashedPassword,
+      authenticationType: 'local'
+    });
+
+    // Crear un carrito vacío para el usuario
+    const cart = await cartModel.create({ products: [] });
+
+    // Asociar el carrito al usuario
+    user.cartId = cart._id;
+    await user.save();
+
+    res.render('home', { title: 'Página de inicio', success: 'Usuario creado exitosamente', error: null });
+  } catch (error) {
+    console.error('Error en el registro de usuarios:', error);
+    res.status(500).render('home', { title: 'Página de inicio', error: 'Error en el servidor', success: null });
+  }
+}
---
+++ Cuarta practica Integradora/src/index.js
@@ -0,0 +1,171 @@
+import "dotenv/config";
+import express from "express";
+import session from 'express-session';
+import { Server } from 'socket.io'
+import { engine } from 'express-handlebars';
+import path from 'path';
+import passport from './controllers/githubAuth.js';
+import cookieParser from 'cookie-parser';
+import MongoStore from 'connect-mongo'
+import { __dirname, __filename } from './path.js'
+import { authenticate } from './middlewares.js';
+import { connectDatabase } from './database.js';
+import { loginUser } from "./controllers/localAuth.js"; 
+import { registerUser } from "./controllers/localAuth.js"; 
+import productRouter from "./routes/product.routes.js";
+import cartRouter from "./routes/cart.routes.js";
+import adminRouter from "./routes/admin.routes.js";
+import premiumRouter from "./routes/premium.routes.js";
+import messagesRouter from "./routes/messages.routes.js";
+import recoveryRouter from "./routes/recovery.routes.js";
+import loggerTestRouter from "./routes/loggerTest.routes.js";
+import { loggerDev, loggerProd } from  "./utils/logger.js";
+import swaggerJsdoc from  'swagger-jsdoc'
+import swaggerUiExpress from 'swagger-ui-express'
+
+const app = express();
+const PORT = 4000;
+
+const server = app.listen(PORT, () => {
+  loggerDev.http(`Server on port ${PORT}`);
+})
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.SIGNED_COOKIE));
+app.use(session({
+  store: MongoStore.create({
+      mongoUrl: process.env.URL_MONGODB_ATLAS,
+      mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true },
+      ttl: 500 // Medido en segundos
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true
+}));;
+
+//ServerIO
+
+const io = new Server(server)
+const mensajes = []
+
+io.on('connection', (socket) => {
+  loggerDev.http(`Ciente conectado socket IO`);
+    socket.on("mensaje", info => {
+      console.log(info)
+        mensajes.push(info)
+        io.emit("mensajes", mensajes) 
+    })
+})
+
+// Handlebars
+app.engine(
+  'handlebars',
+  engine({
+    partialsDir: path.resolve(__dirname, 'views'),
+    runtimeOptions: {
+      allowProtoPropertiesByDefault: true,
+      allowProtoMethodsByDefault: true
+    },
+    helpers: {
+      eq: function (a, b, options) {
+        if (a === b) {
+          return options.fn(this);
+        } else {
+          return options.inverse(this);
+        }
+      }
+    }
+  })
+);
+app.set('view engine', 'handlebars');
+app.set('views', path.resolve(__dirname, 'views'));
+
+// Conexion MongoDB
+connectDatabase();
+
+// Acceso Home
+app.get('/', (req, res) => {
+  res.render('home', {
+    layout: false,
+    partials: {
+      navbar: 'navbar', 
+    },
+    title: 'Página de inicio',
+
+  });
+});
+
+// Ruta POST para el inicio de sesión
+app.post('/login', loginUser);
+
+
+// Ruta POST para el registro de usuarios
+app.post('/registro', registerUser);
+
+// Cerrar sesion (LOGOUT)
+app.get("/logout", (req, res) => {
+
+  // Eliminar la sesión del usuario
+  req.session.destroy();
+  // Redireccionar al inicio de sesión
+  res.redirect("/");
+});
+
+// Configurar las rutas de autenticación de GitHub
+app.get('/auth/github', passport.authenticate('github', { scope: ['user:usernamegithub'] }));
+app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }),
+  (req, res) => {
+    req.session.user = req.user;
+    res.redirect('/products');
+  }
+);
+
+//Swagger configuracion 
+const swaggerOptions={
+  definition:{
+      openapi:'3.0.1',
+      info:{
+          title:" Documentacion de las API",
+          description:" Informacion de tienda virtual",
+          version: '1.0.0',
+          contact:{
+              name:"Francisco Fernandez",
+              url: "https://www.linkedin.com/in/francisco-fernandez-fernandez-97930430/"
+          }
+      }
+  },
+  apis: [`${process.cwd()}/src/docs/**/*.yaml`],
+  // apis: [`./docs/**/*.yaml`],
+}
+
+const spec=swaggerJsdoc(swaggerOptions)
+
+// Rutas con permisos de acceso de usuarios segun rol
+app.use('/products', authenticate(['usuario', 'premium']), productRouter);
+app.use('/carts', authenticate(['usuario', 'premium']), cartRouter);
+app.use('/admin', authenticate(['administrador']), adminRouter);
+app.use('/premium', authenticate(['premium']), premiumRouter);
+app.use('/message',authenticate(['usuario', 'premium','administrador']), messagesRouter);
+app.use('/chat',authenticate(['usuario', 'premium','administrador']), express.static(__dirname + '/public')) 
+app.use('/recovery', recoveryRouter);
+app.use('/apidocs',swaggerUiExpress.serve,swaggerUiExpress.setup(spec))
+
+//vista chat
+app.get("/chat", (req, res) => {
+  const cartId = req.session.user.cartId; 
+  const userEmail = req.session.user.email; 
+  console.log("Se accedió a la vista de chat. Usuario:", userEmail);
+  loggerDev.http(`Ciente ${userEmail} acaba de conectarse a chat`);
+  res.render('chat', { cartId: cartId }); 
+});
+
+// Agregar la ruta de prueba del logger
+app.use("/loggerTest", loggerTestRouter);
+
+
+
+
+
+
+
